7e92a37d07df81889b20f4c95d227581f5ae120d
Phong Nguyen <pnguyenduong@gmail.com>
2023-01-13 19:48:51 -0600

    added entry widget

--- chat.py
+++ chat.py
@@ -19,7 +19,7 @@ frame = customtkinter.CTkFrame(master=root,
                                width=400,
                                height=300,
                                corner_radius=10)
-frame.pack(padx=(20, 100), pady=(300, 20))
+frame.pack(padx=(20), pady= (20))
 
 # Add Text Widget to get ChatGPT Responses
 textbox = customtkinter.CTkTextbox(frame)
@@ -31,6 +31,15 @@ textbox.configure(
     fg_color='#d6d6d6',
     wrap=WORD,
     state='normal')
+
+# Entry Widget to type stuff into ChatGPT
+chat_entry = customtkinter.CTkEntry(root, 
+    placeholder_text='Type something to OpenAI',
+    width=600,
+    height=50,
+    border_width=2)
+chat_entry.pack(pady=10)
+
 
 # Run the app
 root.mainloop()
