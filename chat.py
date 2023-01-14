6a4faa3b8f5e6979327cbe5057d9d824d11f398d
Phong Nguyen <pnguyenduong@gmail.com>
2023-01-13 19:42:51 -0600

    added text widget

--- chat.py
+++ chat.py
@@ -9,8 +9,28 @@ import pickle
 root = customtkinter.CTk()
 root.title('Chatopia')
 root.geometry('800x600')
-root.iconbitmap('ai_lt.ico')
 
 # Set Color Scheme
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('dark-blue')
+
+# Create Text Frame
+frame = customtkinter.CTkFrame(master=root,
+                               width=400,
+                               height=300,
+                               corner_radius=10)
+frame.pack(padx=(20, 100), pady=(300, 20))
+
+# Add Text Widget to get ChatGPT Responses
+textbox = customtkinter.CTkTextbox(frame)
+textbox.grid(row=0, column=0)
+textbox.configure(
+    text_color='black',
+    width=600,
+    height=400,
+    fg_color='#d6d6d6',
+    wrap=WORD,
+    state='normal')
+
+# Run the app
+root.mainloop()
